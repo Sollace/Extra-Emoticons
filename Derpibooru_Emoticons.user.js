073427c45cded8f64889df6e71031821d122b6d7
Sollace <mwchris@mweb.co.za>
2018-01-29 22:49:17 +0200

    JS Update Fixed various bugs with the derpi version

--- Derpibooru_Emoticons.user.js
+++ Derpibooru_Emoticons.user.js
@@ -3,61 +3,47 @@
 // @name        Derpibooru Emoticons
 // @description Adds emoticons to derpibooru.org.
 // @namespace   sollace
-// @include     /^http?[s]://(derpi|trixie)boo(\.ru|ru\.org).*/
-// @version     1.2.1
+// @include     /^http?[s]://(derpi|trixie)booru\.org.*/
+// @version     1.2.2
 // @grant       none
 // ==/UserScript==
 
-var version = '1';
-var taken = [];
-var emoticons = [];
+const version = '1.2.1';
+const taken = [];
+const emoticons = [];
 
-var make = (function() {
-  var div = document.createElement('DIV');
-  function make(htm, context) {
+const make = (function() {
+  const div = document.createElement('DIV');
+  return function make(htm, context) {
     context = context || div;
     context.innerHTML = htm;
     htm = [].slice.apply(context.childNodes);
     context.innerhHTMl = '';
     return htm;
   }
-  make.in = function(holder) {
-    holder = make(holder, div)[0];
-    return function(htm) {
-      return make(htm, holder);
-    };
-  };
-  return make;
 })();
 function each(arr, func) {
-  for (var i = arr.length; i--;) func.apply(arr[i], [arr, i]);
-  return arr;
+  return [].forEach.call(arr, func);
 }
-function timeoutOn(sender, func, time) {
-  return setTimeout(function() {
-    return func.apply(sender, arguments);
-  }, time);
+function all(selector, func) {
+  return each(document.querySelectorAll(selector), func);
 }
 
 function Emoticon(htm, item, name, id) {
-  var name_d = ':' + Emoticon.resolve(name) + ':';
+  let name_d = `:${Emoticon.resolve(name)}:`;
   taken.push(name_d);
-  this.html = '<a class="emote" title="' + name_d + '" style="background-image:url(' + item.split('|')[0] + ');" ><img title=":' + name + ':" src="' + item.split('|')[0] + '"></img></a>'
+  this.html = `<a class="emote" title="${name_d}" style="background-image:url(${item.split('|')[0]});" ><img title=":${name}:" src="${item.split('|')[0]}"></img></a>`;
   this.category = id;
   this.from = name_d;
-  //if (item.indexOf('derpicdn.') != -1) {
-  //  this.to = '>>' + item.split('/').reverse()[0].split(/[^0-9]/)[0] + 't';
-  //} else {
-    this.to = '!' + item.split('|')[0] + '!';
-  //}
+  this.to = `!${item.split('|')[0]}!`;
   htm.push(this.html);
   emoticons.push(this);
 }
 Emoticon.resolve = function(name_d) {
-  if (taken.indexOf(name_d) != -1) {
-    var mix = 1;
-    while (taken.indexOf(name_d + '_' + mix) != -1) mix++;
-    return name_d + '_' + mix;
+  if (taken.indexOf(name_d) > -1) {
+    let mix = 1;
+    while (taken.indexOf(`${name_d}_${mix}`) > -1) mix++;
+    return `${name_d}_${mix}`;
   }
   return name_d;
 };
@@ -71,33 +57,19 @@ Emoticon.prototype = {
 }
 
 function transformText(text, func) {
-  var mode = false;
-  return each(text.split(/(==|@)/), function(arr, index) {
+  let mode = false;
+  return text.split(/(==|@)/).map((a, index, arr) => {
     if (!mode) {
-      if (this == '==' || this == '@') {
-        return mode = this.toString();
+      if (a == '==' || a == '@') {
+        mode = a.toString();
+        return a;
       }
-      arr[index] = func(this);
+      return func(a);
     } else {
-      if (this == mode) {
-        mode = false;
-      }
+      if (a == mode) mode = false;
     }
+    return a;
   }).join('');
-}
-
-function configToHtm(config) {
-  var htm = [];
-  if (config.length) {
-    each(config, function() {
-      new Emoticon(htm, this, this.split('|').reverse()[0].split(/[^a-zA-Z0-9]/)[0]);
-    });
-  } else {
-    each(config.emotes, function() {
-      new Emoticon(htm, config.url.replace('{name}', this), this);
-    });
-  }
-  return htm.join('');
 }
 
 function emotesOf(obj) {
@@ -106,39 +78,29 @@ function emotesOf(obj) {
 
 function complexConfigToHtm(config) {
   var htm = [];
-  each(config, function() {
-    var url = this.url || '{name}';
-    if (this.cases) {
-      each(this.cases, function() {
-        var cas = this.case || false;
-        each(emotesOf(this), function() {
-          var splitten = (this.emote || this).split('|');
-          new Emoticon(htm, url.replace('{name}', splitten[0]), splitten.reverse()[0], this.case || cas);
-        });
+  config.forEach(group => {
+    const url = group.url || '{name}';
+    (group.cases || [group]).forEach(cases => {
+      const cas = cases.case || false;
+      emotesOf(cases).forEach(item => {
+        const splitten = (item.emote || item).split('|');
+        new Emoticon(htm, url.replace('{name}', splitten[0]), splitten.reverse()[0], item.case || cas);
       });
-    } else {
-      var cas = this.case || false;
-      each(emotesOf(this), function() {
-        var splitten = (this.emote || this).split('|');
-        new Emoticon(htm, url.replace('{name}', splitten[0]), splitten.reverse()[0], this.case || cas);
-      });
-    }
+    });
   });
   return htm.join('');
 }
 
 function dragStart(event) {
-  var data = event.dataTransfer.getData('Text/plain');
+  let data = event.dataTransfer.getData('Text/plain');
   if (data && data.trim().indexOf('[') == 0) {
-    data = data.split(/\n| /g);
-    for (var i = data.length; i--;) {
-      data[i] = data[i].trim().replace(/\[/g, '').replace(/\]/g, '');
-    }
-    event.dataTransfer.setData('Text/plain', data.join(''));
+    data = data.split(/\n| /g).map(a => a.trim().replace(/\[/g, '').replace(/\]/g, '')).join('');
   } else {
-    event.dataTransfer.setData('Text/plain', this.getAttribute('title'));
+    data = this.getAttribute('title');
   }
+  event.dataTransfer.setData('Text/plain', data);
 }
+
 function insertTags(textarea, open, close) {
   var start = textarea.selectionStart;
   if (start || start == 0) {
@@ -158,15 +120,15 @@ function insertTags(textarea, open, close) {
   }
 }
 
-var focused_el = null;
+let focused_el = null;
 
 function focusable(el) {
   if (el && !el.hasFocus) {
-    el.addEventListener('focus', function() {
-      if (focused_el != this) focused_el = this;
+    el.addEventListener('focus', () => {
+      if (focused_el != el) focused_el = el;
     });
-    el.addEventListener('blur', function() {
-      if (focused_el == this) focused_el = null;
+    el.addEventListener('blur', () => {
+      if (focused_el == el) focused_el = null;
     });
     el.hasFocus = function() {
       return focused_el == this;
@@ -201,15 +163,15 @@ function readscroll(sender, reciever) {
 }
 
 function clearSelection() {
-  each(document.querySelectorAll('.selection-start, .selection-end'), function() {
-    this.className = this.className.replace(' selection-start', '').replace(' selection-end','');
+  all('.selection-start, .selection-end', a => {
+    a.classList.remove('selection-start');
+    a.classList.remove('selection-end');
   });
 }
 
 function updateSelection() {
   clearSelection();
-  var selection = window.getSelection();
-  var range = selection.getRangeAt(0);
+  const range = window.getSelection().getRangeAt(0);
   if (range.startContainer) {
     range.startContainer.className += ' selection-start';
     range.endContainer.className += ' selection-end';
@@ -218,24 +180,18 @@ function updateSelection() {
 
 function CommentBox(element) {
   this.dom = focusable(element);
-  this.dum = focusable(make('<textarea class="dummy ' + this.dom.getAttribute('class').replace(' js-preview-input', '') + '" placeholder="' + this.dom.getAttribute('placeholder') + '" />')[0]);
+  this.dum = focusable(make(`<textarea class="dummy ${this.dom.getAttribute('class').replace(' js-preview-input', '')}" placeholder="${this.dom.getAttribute('placeholder')}" />`)[0]);
   this.dom.parentNode.appendChild(this.dum);
   if (!this.dom) return;
   this.dom.className += ' comment_box';
   this.dom.parentNode.className += ' comment_box_parent';
-  timeoutOn(this, function() {
-    this.returnAliases(this.dom);
-  }, 1);
+  requestAnimationFrame(() => this.returnAliases(this.dom));
   this.ready = true;
   var me = this;
   this.form = this.dom.form;
-  this.form.addEventListener('submit', function(e) {
-    me.submit();
-  });
+  this.form.addEventListener('submit', e => me.submit());
   readscroll(this.dom, this);
-  this.dum.addEventListener('mouseenter', function() {
-    me.unsubmit();
-  });
+  this.dum.addEventListener('mouseenter', () => me.unsubmit());
   this.pane = make('<div id="comment_emotes"></div>')[0];
   this.dom.parentNode.appendChild(this.pane);
   this.pane.addEventListener('mousedown', function(e) {
@@ -256,31 +212,26 @@ function CommentBox(element) {
   });
   this.preview = document.querySelector('a[data-click-tab="preview"]');
   if (this.preview) {
-    this.preview.addEventListener('mouseenter', function() {
-      me.submit();
-    });
-    this.preview.addEventListener('mouseleave', function() {
-      me.unsubmit();
-    });
+    this.preview.addEventListener('mouseenter', () => me.submit());
+    this.preview.addEventListener('mouseleave', () => me.unsubmit());
   }
-  document.addEventListener('mousedown', function(e) {
+  document.addEventListener('mousedown',e => {
     clearSelection();
-    var target = e.target || e.srcElement;
-    if (target.className.indexOf('post-reply-quote') != -1) {
-      var string = target.getAttribute('data-post');
-      each(emoticons, function() {
-        string = this.returnAlias(string);
-      });
-      target.setAttribute('data-post', string);
-    }
+    const target = e.target.closest('.post-reply-quote');
+    if (!target) return;
+    let string = target.dataset.post;
+    emoticons.forEach(emoticon => {
+      string = emoticon.returnAlias(string);
+    });
+    target.dataset.post = string;
   });
 }
 CommentBox.prototype = {
   submit: function() {
-    if (this.form.getAttribute('data-submitting') !== 'true') {
+    if (this.form.dataset.submitting !== 'true') {
       readscroll(this.dom, this);
       this.dum.value = this.replaceAliases(this.dom);
-      this.form.setAttribute('data-submitting', 'true');
+      this.form.dataset.submitting = 'true';
       swap(this.dom, this.dum);
       this.returnAliases(this.dum);
       readscroll(this, this.dum);
@@ -292,18 +243,16 @@ CommentBox.prototype = {
     if (this.dum.value) {
       readscroll(this.dum, this);
       this.dom.value = this.returnAliases(this.dum);
-      this.form.setAttribute('data-submitting', 'false');
+      this.form.dataset.submitting = 'false';
       swap(this.dum, this.dom);
       readscroll(this, this.dom);
     }
   },
   replaceAliases: function(dom) {
-    var string = dom.value;
+    let string = dom.value;
     if (string) {
-      string = transformText(string, function(part) {
-        each(emoticons, function() {
-          part = this.replaceAlias(part);
-        });
+      string = transformText(string, part => {
+        emoticons.forEach(emote => part = emote.replaceAlias(part));
         return part;
       });
       dom.value = string;
@@ -311,12 +260,10 @@ CommentBox.prototype = {
     return dom.value || '';
   },
   returnAliases: function(dom) {
-    var string = dom.value;
+    let string = dom.value;
     if (string) {
-      string = transformText(string, function(part) {
-        each(emoticons, function() {
-          part = this.returnAlias(part);
-        });
+      string = transformText(string, part => {
+        emoticons.forEach(a => part = a.returnAlias(part));
         return part;
       });
       dom.value = string;
@@ -324,127 +271,89 @@ CommentBox.prototype = {
     return dom.value || '';
   },
   addEmoticons: function(htm) {
-    var pane = this.pane;
-    each(make(htm), function() {
-      this.childNodes[0].addEventListener('dragstart', dragStart);
-      pane.appendChild(this);
+    this.pane.innerHTML = htm;
+    each(this.pane.children, a => {
+      a.childNodes[0].addEventListener('dragstart', dragStart);
     });
   }
 };
 
-var ExtraEmotes = (function(win) {
+const ExtraEmotes = (function(win) {
   var modules = [];
   try {
-    each(document.querySelectorAll('#comment_body, #post_body, #description, #image_description, #message_body, #topic_posts_attributes_0_body, textarea#body'), function() {
-      modules.push(new CommentBox(this));
+    all('#comment_body, #post_body, #description, #image_description, #message_body, #topic_posts_attributes_0_body, textarea#body', a => {
+      modules.push(new CommentBox(a));
     });
   } catch (e) {
     console.log(e);
   }
   return win.ExtraEmotes = {
-    addEmoticons: function(id, name, _title_, emotes, _normalize_, _buttonImage_) {
-      this.load(name ? { id: id, emotes: emotes } : id)
+    addEmoticons: function(id, name, _title_, emotes) {
+      this.load(name ? { id: id, emotes: emotes } : id);
     },
     load: function(config) {
       if (!this.ready()) return;
-      var htm = complexConfigToHtm(config);
-      each(modules, function() {
-        this.addEmoticons(htm);
-      });
+      const htm = complexConfigToHtm(config);
+      modules.forEach(module => module.addEmoticons(htm));
     },
     ready: function() {
       return modules.length > 0;
     },
     getVersion: function() {
       return version;
-    },
-    valueOf: function valueOf() {
-      return this.toString();
-    },
-    toString: function toString() {
-      return '[object API] {\n  addEmoticons(id, name, title, emotes, normalize [, buttonImage]))';
     }
   };
 })(typeof (unsafeWindow) !== 'undefined' && unsafeWindow != window ? unsafeWindow : window);
+
 if (ExtraEmotes.ready()) {
-  document.head.appendChild(make('<style type="text/css">\
-#comment_emotes {\
-  box-sizing: border-box;\
-  display: inline-block;\
-  overflow-y: auto;\
-  width: 36.666%;\
-  height: 330px;\
-  vertical-align: top;\
-  border: 1px solid #cdcdcd;\
-  border-left: none;\
-  text-align: center;\
-  padding: 5px;\
-}\
-.comment_box, .comment_box + textarea {\
-  display: inline-block;\
-  max-width: 60%;\
-  min-height: 330px;\
-}\
-form[data-submitting="true"] .comment_box,\
-.comment_box + textarea {\
-  display: none;\
-}\
-.comment_box:-moz-ui-invalid:not(output) + textarea,\
-.comment_box:-moz-ui-invalid:not(output) ~ #comment_emotes {\
-  box-shadow: 0 0 1.5px 1px red;\
-}\
-form[data-submitting="true"] .comment_box + textarea {\
-  display: inline-block;\
-}\
-a.emote, a.emote img {\
-  -webkit-backface-visibility: hidden;\
-  -webkit-transform: translateZ(0) scale(1.0, 1.0);\
-  transform: translateZ(0);\
-}\
-a.emote img {\
-  opacity: 0;\
-}\
-a.emote {\
-  vertical-align: top;\
-  display: inline-block;\
-  padding: 6px;\
-  border-radius: 100%;\
-  width: 27px;\
-  height: 27px;\
-  transition: background 0.2s ease, transform 0.1s ease;\
-  background: no-repeat center;\
-  transform: scale(1,1) rotate(0) translateZ(0);\
-}\
-a.emote {\
-  image-rendering: optimizeSpeed;             /*                     */\
-  image-rendering: -moz-crisp-edges;          /* Firefox             */\
-  image-rendering: -o-crisp-edges;            /* Opera               */\
-  image-rendering: -webkit-optimize-contrast; /* Chrome (and Safari) */\
-  image-rendering: optimize-contrast;         /* CSS3 Proposed       */\
-  -ms-interpolation-mode: nearest-neighbor;   /* IE8+                */\
-}\
-a.emote:hover img {\
-  opacity: 0.7 !important;\
-  filter: blur(0.3px);\
-}\
-a.emote:hover, a.emote.selection-start, a.emote.selection-start ~ a {\
-  background-color: rgba(220,220,220,0.4);\
-  transform: scale(1.1,1.1) rotate(10deg) translateZ(0);\
-}\
-a.emote:nth-child(odd):hover, a.emote:nth-child(odd).selection-start, a.emote.selection-start ~ a:nth-child(odd) {\
-  transform: scale(1.1,1.1) rotate(-10deg) translateZ(0);\
-}\
-a.emote.selection-end ~ a, a.emote.selection-end ~ a:nth-child(odd) {\
-  background-color: transparent;\
-  transform: scale(1,1) rotate(0) translateZ(0);\
-}\
-a.emote:active {\
-  transform: scale(1,1) rotate(10deg) translateZ(0);\
-}\
-a.emote:nth-child(odd):active {\
-  transform: scale(1,1) rotate(-10deg) translateZ(0);\
-}\
-</style>')[0]);
+  document.head.insertAdjacentHTML('beforeend', `<style type="text/css">
+#comment_emotes {
+  box-sizing: border-box;
+  display: inline-block;
+  overflow-y: auto;
+  width: 36.666%;
+  height: 330px;
+  vertical-align: top;
+  border: 1px solid #cdcdcd;
+  border-left: none;
+  text-align: center;
+  padding: 5px;}
+.comment_box, .comment_box + textarea {
+  display: inline-block;
+  max-width: 60%;
+  min-height: 330px;}
+form[data-submitting="true"] .comment_box,
+.comment_box + textarea {display: none;}
+form[data-submitting="true"] .comment_box + textarea { display: inline-block;}
+a.emote, a.emote img {
+  -webkit-backface-visibility: hidden;
+  -webkit-transform: translateZ(0) scale(1.0, 1.0);
+  transform: translateZ(0);}
+a.emote img {opacity: 0;}
+a.emote {
+  vertical-align: top;
+  display: inline-block;
+  padding: 6px;
+  border-radius: 100%;
+  width: 27px;
+  height: 27px;
+  transition: background 0.2s ease, transform 0.1s ease;
+  background: no-repeat center;
+  transform: scale(1,1) rotate(0) translateZ(0);}
+a.emote:hover img {
+  opacity: 0.7 !important;
+  filter: blur(0.3px);}
+a.emote:hover, a.emote.selection-start, a.emote.selection-start ~ a {
+  background-color: rgba(220,220,220,0.4);
+  transform: scale(1.1,1.1) rotate(10deg) translateZ(0);}
+a.emote:nth-child(odd):hover, a.emote:nth-child(odd).selection-start, a.emote.selection-start ~ a:nth-child(odd) {
+  transform: scale(1.1,1.1) rotate(-10deg) translateZ(0);}
+a.emote.selection-end ~ a, a.emote.selection-end ~ a:nth-child(odd) {
+  background-color: transparent;
+  transform: scale(1,1) rotate(0) translateZ(0);}
+a.emote:active {transform: scale(1,1) rotate(10deg) translateZ(0);}
+a.emote:nth-child(odd):active {transform: scale(1,1) rotate(-10deg) translateZ(0);}
+</style>`);
 }
 
 ExtraEmotes.load([
